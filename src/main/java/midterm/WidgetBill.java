e4f93a6e47225500de9a18fdfb2d6b6d4d91f3a8
Douglas Luman <dluman@douglasjluman.com>
2020-03-01 21:40:56 -0500

    Also, stop messing up the grader, Luman

--- src/main/java/midterm/WidgetBill.java
+++ src/main/java/midterm/WidgetBill.java
@@ -21,6 +21,7 @@ public class WidgetBill {
     Widget gadget = new Widget("A Gadget",119.33,41.12);
     // Widgets ---------------------------------------------------------
     
+    // Print the widgets
     System.out.println(thingamajig);
     System.out.println(zonk);
     System.out.println(maguffin);
@@ -28,22 +29,27 @@ public class WidgetBill {
     System.out.println(gadget);
     System.out.println();
     
+    // Create total price
     double totalPrice = thingamajig.getPrice() + zonk.getPrice() + maguffin.getPrice()
                         + whatchamacallit.getPrice() + gadget.getPrice();
    
+    // Get discount rate
     Scanner input = new Scanner(System.in);
     System.out.print("Enter member discount rate: ");
     double discountRate = input.nextDouble();
     
+    // Print the total price and discount rate
     System.out.println();
     System.out.println("Price before discount: " + totalPrice);
     totalPrice -= totalPrice * discountRate / 100;
     System.out.println("Member discount applied: " + discountRate + "%");
     
+    // Get total weight and apply shipping
     double totalWeight = thingamajig.getWeight() + zonk.getWeight() + maguffin.getWeight()
                          + whatchamacallit.getWeight() + gadget.getWeight();
     double shippingCost = .33 * totalWeight;
     
+    // Print final information
     System.out.println("Final price: " + totalPrice + ", Total weight: " + totalWeight);
     System.out.println("Cost to ship: " + shippingCost);
     System.out.print("Final total: " + (totalPrice + shippingCost));
